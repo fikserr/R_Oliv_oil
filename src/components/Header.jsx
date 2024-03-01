24ac521ae6de02c0f36c753635ce1f3a19951347
fikserr <azizxon7777777@gmail.com>
2024-03-01 18:44:42 +0500

    load

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,44 +1,102 @@
-import Italianskoe from "../img/dlia_salata.png";
+import salat from "../img/dlia_salata.png";
+import italianskoe from "../img/italianskoe.png";
+import forMeat from "../img/dlia_myasa.png";
+import Eastern from "../img/vostochnoe.png";
 import Potato from "../img/image_12.png";
+import lagmon from "../img/image_16.png";
+import meat from "../img/image_14.png";
+import plov from "../img/image_15.png";
+import { MdNavigateNext, MdNavigateBefore } from "react-icons/md";
 
+
+const data = [
+  {
+    id:1,
+    oilImg: salat,
+    fruit: Potato,
+    productTitle: "Наши продукты",
+    title: `Масло “Для салата”`,
+    text: "Название говорит само за себя. Это идеальная заправка, которая даже скучной и привычной капусте придаст неповторимый вкус и аромат. ",
+
+  },
+  {
+    id:2,
+    oilImg: italianskoe,
+    fruit: lagmon,
+    productTitle: "Наши продукты",
+    title: `Масло “Итальянское”`,
+    text: "Идеальное решение для легкого и быстрого маринада мяса, можно добавить капельку масла и после приготовления. ",
+
+  },
+  {
+    id:3,
+    oilImg: forMeat,
+    fruit: meat,
+    productTitle: "Наши продукты",
+    title: `Масло “Для мяса”`,
+    text: "Идеальное решение для легкого и быстрого маринада мяса, можно добавить капельку масла и после приготовления. ",
+
+  },
+  {
+    id:4,
+    oilImg: Eastern,
+    fruit: plov,
+    productTitle: "Наши продукты",
+    title: `Масло “Восточное”`,
+    text: "Идеальное решение для легкого и быстрого маринада мяса, можно добавить капельку масла и после приготовления. ",
+
+  }
+]
 
 function Header() {
   return (
     <header>
             <div className="container">
 
-            <div className="header__content">
-                                <div className="header__imgs-left">
-                                    <div className="oil__circle"></div>
-                                    <img src={Italianskoe} alt="" className="dlia_salata"/>
-                                    <img src={Potato} alt="" className="potato"/>
-                                </div>
-
-
-                              <div className="header__info-right">
-                                <h2>Наши продукты</h2>
-                                  <div className="head__btn">
-                                      <button className="actived">Для салата</button>
-                                      <button>Итальянское</button>
-                                      <button>Для мяса</button>
-                                      <button>Восточное</button>
-                                  </div>
-                                <h1>Масло “Для салата”</h1>
-                                <p>Название говорит само за себя. Это идеальная заправка, которая даже скучной и привычной капусте придаст неповторимый вкус и аромат. 
-                                </p>
+            
+              {
+                data.map((item)=>{
+                  return <div className="header__content-all" key={item.id}>
+                        <button className="next"><MdNavigateBefore/></button>
+                            <div className="header__content">
                                 
-                                .head__
-
-                                <div className="see">
-                                  <button><a href="">ПОДРОБНЕЕ</a></button>
-                                  <button><a href="">В КОРЗИНУ</a></button>
-                                </div>
-                              </div>
+                                  <div className="header__imgs-left">
+                                    <div className="oil__circle"></div>
+                                    <img src={item.oilImg} alt="" className="header__img-oil"/>
+                                    <img src={item.fruit} alt="" className="header__img-fruit"/>
+                                  </div>
 
 
-                        </div>
-                
+                                  <div className="header__info-right">
+                                    <h2 className="product__title">{item.productTitle}</h2>
+                                      <div className="header__btn">
+                                          <button className="header__btn-txt actived">Для салата</button>
+                                          <button className="header__btn-txt ">Итальянское</button>
+                                          <button className="header__btn-txt ">Для мяса</button>
+                                          <button className="header__btn-txt ">Восточное</button>
+                                      </div>
+                                    <h1 className="header__title">{item.title}</h1>
+                                    <p className="header__text">{item.text}</p>
+                                    
+                                          <div className="head__num">
+                                            <button className="minus">-</button>
+                                              <div className="num">0</div>
+                                            <button className="plus">+</button>
+                                          </div>
 
+                                    <div className="header__links-bottom">
+                                      <button className="header__link"><a href="">ПОДРОБНЕЕ</a></button>
+                                      <button className="header__link"><a href="">В КОРЗИНУ</a></button>
+                                    </div>
+                                  </div>
+                                  
+                            </div>
+                        <button className="back"><MdNavigateNext/></button>
+
+                          </div>
+  
+                })
+              }
 
 
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,5 @@
 
+
 function Main() {
   return (
     <div>Main</div>
